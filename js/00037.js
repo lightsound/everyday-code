ae4a5ccc2996b3ab0f08e17a7e3399e5c4f365af
skoto <0120koto@gmail.com>
2020-06-17 17:44:31 +0900

    :sparkles: Introducing new code.

---
+++ js/00037.js
@@ -0,0 +1,9 @@
+// 🙅 const で宣言したものを書き換えないで
+const obj1 = { name: "しまぶー", univ: "IT大学" };
+obj1.name = "foo";
+console.log(obj1); // { name: "foo", univ: "IT大学" };
+
+// 🙆 新たにコピーして宣言しなおそう
+const obj2 = { name: "しまぶー", univ: "IT大学" };
+const clone = { ...obj2, name: "foo" };
+console.log(clone); // { name: "foo", univ: "IT大学" };
