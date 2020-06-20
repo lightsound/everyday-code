17555e0b7d437e4c38aff24885874133554db0a0
skoto <0120koto@gmail.com>
2020-06-20 17:05:50 +0900

    :recycle: Refactoring code.

--- js/00036.js
+++ js/00036.js
@@ -2,5 +2,6 @@ const obj = { name: "しまぶー" };
 const clone = { ...obj }; // { name: "しまぶー" };
 console.log(obj === clone); // false
 
-clone.name = "foo"; // ← そもそもこの式もBAD。後の授業で説明します。
+// 下記の式は本当はBADです。次回より良い方法を解説します。
+clone.name = "foo";
 console.log(obj); // { name: "しまぶー" }; ☺️ 安心
